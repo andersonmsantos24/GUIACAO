5f0048a4879e6f72b0202ad5c29b7b0e0b8e2217
andersonmsantos24 <anderson.m.santos24@aluno.senai.br>
2025-09-30 18:17:04 -0300

    Delete guiacao directory

--- guiacao/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'guiacao');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DOG_API_KEY', 'live_MQfHWKhVRUM6F88u307b1FGpZ8hgDsRekxgAhfruIyiiXtRXSfWRYf5j2tjj6mbj'); 
